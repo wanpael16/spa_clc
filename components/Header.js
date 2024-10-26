b097b13b904ae766f1b8233fb0a9860c7f1dcfbf
Walter <wanpael16@gmail.com>
2024-10-26 12:50:39 -0500

    commit 10

--- components/Header.js
+++ components/Header.js
@@ -24,8 +24,8 @@ const Header = () => {
       <header className="container-header">
         <Swiper
           style={{
-            "--swiper-navigation-color": "#171717",
-            "--swiper-pagination-color": "#171717",
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#777070",
           }}
           modules={[Autoplay, Navigation, Pagination]}
           autoplay={{
@@ -38,26 +38,22 @@ const Header = () => {
           onAutoplayTimeLeft={onAutoplayTimeLeft}
         >
           <SwiperSlide>
-           
-              <Image
-                src="/img/doctor-nurses-special-equipment.jpg" // Cambia por la ruta de tu imagen
-                alt="Imagen de fondo"
-                fill={true}
-                style={{ objectFit: "cover" }}
-                priority={true} // Para optimizar la carga de la imagen
-              />
-          
+            <Image
+              src="/img/doctor-nurses-special-equipment.jpg" // Cambia por la ruta de tu imagen
+              alt="Imagen de fondo"
+              fill={true}
+              style={{ objectFit: "cover" }}
+              priority={true} // Para optimizar la carga de la imagen
+            />
           </SwiperSlide>
           <SwiperSlide>
-            
-              <Image
-                src="/img/doctor-nurses-special-equipment.jpg" // Cambia por la ruta de tu imagen
-                alt="Imagen de fondo"
-                fill={true}
-                style={{ objectFit: "cover" }}
-                priority={true} // Para optimizar la carga de la imagen
-              />
-           
+            <Image
+              src="/img/doctor-nurses-special-equipment.jpg" // Cambia por la ruta de tu imagen
+              alt="Imagen de fondo"
+              fill={true}
+              style={{ objectFit: "cover" }}
+              priority={true} // Para optimizar la carga de la imagen
+            />
           </SwiperSlide>
 
           <div className="autoplay-progress">
@@ -68,64 +64,110 @@ const Header = () => {
           </div>
         </Swiper>
 
-        <div className=" contain-nav">
-          <div className="main-container">
-            <div className="contain-nav-start">
-              <div className="contain-nav-start__image-Container-logo">
-                <img src="/img/logoBlanco.png" alt="" />
+        <div className="contain-nav">
+          <div className="grid-nav-start">
+            <div className="grid-nav-start_contain-nav">
+              <div className="contain-nav-start">
+                <div className="contain-nav-start__image-Container-logo">
+                  <img src="/img/logoBlanco.png" alt="" />
+                </div>
+                <div className="contenedor-info">
+                  <div className="contain-contact">
+                    <div className="contain-contact__img">
+                      <img src="/img/clock-countdown.png" alt="" />
+                    </div>
+                    <div className="contain-contact__hours">
+                      <span>Lunes a Domingo</span>
+                      <span>24horas</span>
+                    </div>
+                  </div>
+                  <div className="contain-phone">
+                    <div className="contain-phone__img">
+                      <img src="/img/phone.png" alt="" />
+                    </div>
+                    <div className="contain-phone__numbers">
+                      <span>(073) 331811 - (073) 363576</span>
+                      <span>Central Telefónica</span>
+                    </div>
+                  </div>
+                  {/* <div className="contain-social-networks">
+                    <div className="contain-social-networks__img">
+                      <img src="/img/facebook_logo.svg" alt="" className="ab" />
+                      <img src="/img/facebook_logo.svg" alt="" />
+                    </div>
+
+                  
+                  </div> */}
+                </div>
               </div>
-              <div className="contenedor-info">
-                <div className="contain-contact">
-                  <div className="contain-contact__img">
-                    <img src="/img/clock-countdown.png" alt="" />
+
+              <nav className="contain-nav-end">
+              <ul>
+                <li>Nosotros</li>
+                <li>Servicios</li>
+                <li>Staff Médico</li>
+                <li>Especialidades</li>
+              </ul>
+          
+              <div>
+              <button className="contain-nav-end__button">
+                <img src="/img/calendar-dots.png" alt="" />
+              <span>RESERVAR CITA</span></button>
+              </div>
+            
+              
+            </nav>
+           </div>
+          </div>
+          <div className="grid-nav-end main-container">
+            <div className="service-nav">
+              
+              <div className="service-nav-content">
+
+                <div className="content-service content-service--filter">
+                  <div className="content-service__caja">
+                    
+                    <div className="content-service__caja-groud">
+                    <img src="/img/stethoscope_b34a91yk2674.svg" alt="" />
+                    <span>Atención de emergencia</span>
+                    </div>
+
+                  </div>
+                  <div className="content-service__caja">
+                    
+                    <div className="content-service__caja-groud">
+                    <img src="/img/menssage.svg" alt="" />
+                    <span>Servicio en línea</span>
+                    </div>
+
                   </div>
 
-                  <div className="contain-contact__hours">
-                    <span>Lunes a Domingo</span>
-                    <span>24horas</span>
-                  </div>
-                </div>
+                  <div className="content-service__caja">
+                    
+                    <div className="content-service__caja-groud">
+                    <img src="/img/doctors.svg" alt="" />
+                    <span>Médicos calificados</span>
+                    </div>
 
-                <div className="contain-phone">
-                  <div className="contain-phone__img">
-                    <img src="/img/phone.png" alt="" />
                   </div>
 
-                  <div className="contain-phone__numbers">
-                    <span>(073) 331811 - (073) 363576</span>
-                    <span>Central Telefónica</span>
-                  </div>
-                </div>
-                {/* <div className="contain-social-networks">
-                  <div className="contain-social-networks__img">
-                    <img src="/img/facebook_logo.svg" alt="" className="ab" />
-                    <img src="/img/facebook_logo.svg" alt="" />
-                  </div>
+                  <div className="content-service__caja">
+                    
+                    <div className="content-service__caja-groud">
+                    <img src="/img/ambulancia.svg" alt="" />
+                    <span>Servicio 24 hora</span>
+                    </div>
 
+                  </div>
+                
                  
-                </div> */}
+                </div>
               </div>
             </div>
-           
-           {/* <nav className="contain-nav-end">
-            <ul>
-              <li>Nosotros</li>
-              <li>Servicios</li>
-              <li>Staff Médico</li>
-              <li>Especialidades</li>
-            </ul>
-         
-             <div>
-             <button className="contain-nav-end__button">
-              <img src="/img/calendar-dots.png" alt="" />
-             <span>RESERVAR CITA</span></button>
-             </div>
-           
-            
-           </nav> */}
-          
           </div>
         </div>
+
+      
       </header>
     </>
   );
